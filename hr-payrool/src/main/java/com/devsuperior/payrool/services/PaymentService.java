8ad598b15fbdf10ac4de351a0ca4e1d24b7287fd
Ruben Feiteira <rubenfeiteira@gmail.com>
2022-09-10 16:09:02 +0200

    project hr-worker created

---
+++ hr-payrool/src/main/java/com/devsuperior/payrool/services/PaymentService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.payrool.services;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.devsuperior.entities.Payment;
+import com.devsuperior.entities.Worker;
+import com.devsuperior.payrool.feignclients.WorkerFeignClient;
+
+
+
+@Service
+public class PaymentService {
+
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+
+	public Payment getPayment(long workerId, int days) {
+		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+	}
+}
