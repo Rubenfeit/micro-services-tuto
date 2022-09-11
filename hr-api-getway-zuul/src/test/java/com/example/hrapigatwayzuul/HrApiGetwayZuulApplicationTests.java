8c2a1a29f9eafbb6de122291702a0427594d2a9c
Ruben Feiteira <rubenfeiteira@gmail.com>
2022-09-11 17:00:34 +0200

    hystrix fault tolerance timeou

---
+++ hr-api-getway-zuul/src/test/java/com/example/hrapigatwayzuul/HrApiGetwayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrapigatwayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGetwayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
