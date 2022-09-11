b47109a38199c3920ad6f61f81b8edfc1c575e55
Ruben Feiteira <rubenfeiteira@gmail.com>
2022-09-11 15:45:54 +0200

    Config eureka client

--- hr-payrool/src/main/java/com/devsuperior/payrool/HrPayroolApplication.java
+++ hr-payrool/src/main/java/com/devsuperior/payrool/HrPayroolApplication.java
@@ -2,12 +2,12 @@ package com.devsuperior.payrool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
-@RibbonClient(name = "hr-worker")
 public class HrPayroolApplication {
 
 	public static void main(String[] args) {
